22dee7aef9263d8424772ce833af56ad6067f61f
kotori <1942512905@qq.com>
2023-10-23 11:14:26 +0800

    多线程的原子类

--- p1-w5-d2/src/com/woniuxy/p1/AppDemo1.java
+++ p1-w5-d2/src/com/woniuxy/p1/AppDemo1.java
@@ -1,35 +1,40 @@
 package com.woniuxy.p1;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AppDemo1 {
     public static void main(String[] args) {
         Windows windows = new Windows();
-        new Thread(windows,"窗口1").start();
-        new Thread(windows,"窗口2").start();
-        new Thread(windows,"窗口3").start();
+        new Thread(windows, "窗口1").start();
+        new Thread(windows, "窗口2").start();
+        new Thread(windows, "窗口3").start();
     }
 }
 
-class Windows implements Runnable{
+class Windows implements Runnable {
 
-    int numberOfTickets=100;
+    AtomicInteger numberOfTickets = new AtomicInteger(10000);
 
     @Override
     public void run() {
-        while (true){
-            synchronized (this){
-                if (numberOfTickets>0) {
-                    numberOfTickets--;
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println(Thread.currentThread().getName() + "卖了一张票,还剩"+numberOfTickets+"张票");
-                }else {
-                    System.out.println(Thread.currentThread().getName() + "卖完票了,还剩"+numberOfTickets+"张票");
-                    break;
+        while (true) {
+            int n = numberOfTickets.get();
+            if (n > 0) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+                if (numberOfTickets.compareAndSet(n, n - 1)) {
+                    System.out.println(Thread.currentThread().getName() + "卖了一张票,还剩" + numberOfTickets.get() + "张票");
+                } else {
+                    System.out.println("买票失败");
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + "卖完票了,还剩" + numberOfTickets.get() + "张票");
+                break;
             }
         }
+
     }
 }
